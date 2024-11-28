9b87b1e4b14ced21d0a7034fa728c7e7a36678b4
bhkim66 <bhmklnj63@gmail.com>
2024-11-28 17:14:30 +0900

    querydsl

---
+++ src/test/java/com/bhkim/queryDsl/QueryDslApplicationTests.java
@@ -0,0 +1,39 @@
+package com.bhkim.queryDsl;
+
+import com.bhkim.queryDsl.entity.Hello;
+import com.bhkim.queryDsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QueryDslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query.selectFrom(qHello).fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		//lombok 동작 확인 (hello.getId())
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
+	}
+
+
+}
---
+++ src/test/java/com/bhkim/queryDsl/entity/MemberTest.java
@@ -0,0 +1,65 @@
+package com.bhkim.queryDsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.bhkim.queryDsl.entity.QMember.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;  // 멀티 스레드 상황에서도 문제가 발생하지 않기 때문에 필드 멤버도 빼도됨
+
+
+    @BeforeEach
+    void 멤버_생성() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : members) {
+            System.out.println("member=" + member);
+            System.out.println("-> member.team=" + member.getTeam());
+        }
+    }
+
+    @Test
+    void startQueryDsl() {
+        Member member1 = queryFactory.select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        assertThat(member1.getUsername()).isEqualTo("member1");
+    }
+}
